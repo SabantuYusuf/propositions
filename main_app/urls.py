00b20adfa0f979f806c34651bcdbac90eb957027
desertlp <lorapaine1@gmail.com>
2020-09-08 16:25:30 -0700

    added home/about path

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), #home is a kwarg
     path('about/', views.about, name='about'), #about is a kwarg
+    path('profile/', views.profile, name='profile'),
+    #login
+    #logout
+    #profile
+    #signup 
+    #index (propositoins list)
+    #show (propositoin indv.)
+
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,10 +5,15 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    # return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
+
+def profile(request):
+    return render(request, 'profile.html')
+    # shows if you are going to vote yes/no on each of the propositions
 
 def index(request):
     return HttpResponse('<h1>Propositions Index Page</h1>')
@@ -16,6 +21,3 @@ def index(request):
 def show(request):
     return HttpResponse('<h1>Proposition Show Page</h1>')
 
-def profile(request):
-    return HttpResponse('<h1>Proposition Show Page</h1>')
-    # shows if you are going to vote yes/no on each of the propositions
