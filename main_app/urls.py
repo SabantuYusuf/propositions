af8ed5011930dda6836677736fa0e041eeddb0ca
desertlp <lorapaine1@gmail.com>
2020-09-08 16:54:34 -0700

    templates and routes up

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), #home is a kwarg
-    path('about/', views.about, name='about'), #about is a kwarg
+    path('about/', views.about, name='about'),
+    path('profile/', views.profile, name='profile'), 
+    path('propositions/', views.index, name='index'),
+    path('propositions/<int:proposition_id>', views.show, name='show'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,17 +5,22 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
-
-def index(request):
-    return HttpResponse('<h1>Propositions Index Page</h1>')
-
-def show(request):
-    return HttpResponse('<h1>Proposition Show Page</h1>')
+    return render(request, 'about.html')
 
 def profile(request):
-    return HttpResponse('<h1>Proposition Show Page</h1>')
+    return render(request, 'profile.html')
     # shows if you are going to vote yes/no on each of the propositions
+
+def index(request):
+    return render(request, 'index.html')
+
+def show(request, proposition_id):
+    proposition = Proposition.objects.get(id=proposition_id)
+    context = {
+        'proposition': proposition,
+    }
+    return render(request, 'show.html', context)
+
