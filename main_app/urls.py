e1dc8848f39b7c92ceaa8dc45317d3302ec2af25
desertlp <lorapaine1@gmail.com>
2020-09-09 13:31:49 -0700

    created lp branch

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,14 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), #home is a kwarg
-    path('about/', views.about, name='about'), #about is a kwarg
-    path('profile/', views.profile, name='profile'),
-    #login
-    #logout
-    #profile
-    #signup 
-    #index (propositoins list)
-    #show (propositoin indv.)
+    path('start/', views.start, name='start'),
 
+    path('about/', views.about, name='about'),
+    
 
+    path('propositions/', views.index, name='index'),
+    path('propositions/<int:proposition_id>', views.show, name='show'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Add the following import
 from django.http import HttpResponse
+from .models import Proposition
 
 # Define the home view
 def home(request):
+<<<<<<< HEAD
     # return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+=======
+>>>>>>> master
     return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'about.html')
+
+def start(request):
+    return redirect('show', proposition_id=1)
 
 def profile(request):
     return render(request, 'profile.html')
     # shows if you are going to vote yes/no on each of the propositions
 
 def index(request):
-    return HttpResponse('<h1>Propositions Index Page</h1>')
+    return render(request, 'index.html')
 
-def show(request):
-    return HttpResponse('<h1>Proposition Show Page</h1>')
+def show(request, proposition_id):
+    proposition = Proposition.objects.get(id=proposition_id)
+    context = {
+        'proposition': proposition,
+    }
+    return render(request, 'show.html', context)
 
