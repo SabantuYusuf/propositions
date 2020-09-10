beafa5228993ad5b77b1c10971129504e383a591
Sabantu <Sabantuyusuf@gmail.com>
2020-09-09 20:43:26 -0500

    index

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), #home is a kwarg
-    path('start/', views.start, name='start'),
-
+    path('propositions/', views.start, name='start'),
+    
     path('about/', views.about, name='about'),
     
 
-    path('propositions/', views.index, name='index'),
+    path('propositions/index', views.index, name='index'),
     path('propositions/<int:proposition_id>', views.show, name='show'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
-
-
+from .models import Proposition
 # Add the following import
 from django.http import HttpResponse
-from .models import Proposition
 
 # Define the home view
 def home(request):
@@ -13,10 +11,19 @@ def about(request):
     return render(request, 'about.html')
 
 def start(request):
-    return redirect('propositions', proposition_id=1)
+    return redirect('show', proposition_id=1)
 
 def index(request):
     return render(request, 'index.html')
+
+# def index(request):
+#     proposition = Proposition.whatIs.object.all()
+#     context = {
+#         'Proposition': proposition
+#     }
+#     return render(request, 'proposition')
+
+
 
 def show(request, proposition_id):
     proposition = Proposition.objects.get(id=proposition_id)
